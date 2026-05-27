c1058ae4deceeadfc8e0a1a30a3efdc5288836dd
leti <liloumrs24@gmail.com>
2026-05-27 02:05:04 +0200

    Delete film directory

--- film/sup.php
+++
@@ -1,7 +0,0 @@
-<?php
-$id = mysqli_connect("localhost", "root", "root", "cinema");
-$idfilm = $_GET['idfilm'];
-mysqli_query($id, "DELETE FROM film WHERE idfilm = $idfilm");
-//Redirection vers la page listeFilms.php
-header("location:listeFilms.php");
-?>
